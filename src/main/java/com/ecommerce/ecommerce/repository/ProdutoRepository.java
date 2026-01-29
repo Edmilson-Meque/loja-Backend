42b024d0546c4251db9637674cabf25b5f526d5e
Edmilson Rui Meque <mequeedmilson105@gmail.com>
2026-01-29 15:30:20 +0200

    Update ProdutoRepository.java

--- src/main/java/com/ecommerce/ecommerce/repository/ProdutoRepository.java
+++ src/main/java/com/ecommerce/ecommerce/repository/ProdutoRepository.java
@@ -2,6 +2,14 @@ package com.ecommerce.ecommerce.repository;
 
 import com.ecommerce.ecommerce.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    @Query("SELECT DISTINCT p FROM Produto p LEFT JOIN FETCH p.categoria")
+    List<Produto> findAllComCategoria();
+    long countByCategoriaId(Long categoriaId);
 }
